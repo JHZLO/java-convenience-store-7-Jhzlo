292d09815562ac39ebd78534260885edf7bb13e8
Jhzlo <kjh0010703@naver.com>
2024-11-09 14:33:32 +0900

    feat(output-view): 편의점 안내문 출력

---
+++ src/main/java/store/controller/StoreController.java
@@ -0,0 +1,18 @@
+package store.controller;
+
+import store.view.InputView;
+import store.view.OutputView;
+
+public class StoreController {
+    private InputView inputView;
+    private OutputView outputView;
+
+    public StoreController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        outputView.printHeaderNotice();
+    }
+}
---
+++ src/main/java/store/view/OutputView.java
@@ -0,0 +1,7 @@
+package store.view;
+
+public class OutputView {
+    public void printHeaderNotice(){
+        System.out.println("안녕하세요. W편의점입니다.");
+    }
+}
